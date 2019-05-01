53e1832f41739c59e3be254be4440960b9d136fa
ik-fib <ik11235@gmail.com>
2019-05-02 03:42:32 +0900

    ABC004-B追加

---
+++ src/main/scala/practice/ABC004/B/Main.scala
@@ -0,0 +1,13 @@
+package practice.ABC004.B
+
+object Main extends App {
+  val c0 = scala.io.StdIn.readLine
+  val c1 = scala.io.StdIn.readLine
+  val c2 = scala.io.StdIn.readLine
+  val c3 = scala.io.StdIn.readLine
+
+  println(c3.reverse)
+  println(c2.reverse)
+  println(c1.reverse)
+  println(c0.reverse)
+}
