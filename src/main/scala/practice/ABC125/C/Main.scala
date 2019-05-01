8ee6a0212e72bbe6aa8a8a9051da66dc9673b962
ik-fib <ik11235@gmail.com>
2019-05-01 11:50:03 +0900

    ABC125-C ACコード追加

--- src/main/scala/practice/ABC125/C/Main.scala
+++ src/main/scala/practice/ABC125/C/Main.scala
@@ -1,9 +1,14 @@
 package practice.ABC125.C
 
 object Main extends App {
+
+  import scala.annotation.tailrec
+  import scala.math.max
+
   val n = scala.io.StdIn.readLine.toInt
   val a = scala.io.StdIn.readLine.split(" ").map(_.toInt)
 
+  @tailrec
   def gcd(x: Int, y: Int): Int = {
     if (y == 0) {
       x
@@ -11,19 +16,22 @@ object Main extends App {
       gcd(y, x % y)
   }
 
-  var rr = List(a(0))
-  var ll = List(a.last)
+  var rr = new Array[Int](n)
+  var ll = new Array[Int](n)
+  rr(0) = a(0)
+  ll(n - 1) = a.last
+
   (1 until n).foreach { i =>
-    rr = gcd(rr.head, a(i)) :: rr
+    rr(i) = gcd(rr(i - 1), a(i))
 
-    ll = gcd(ll.head, a(n - 1 - i)) :: ll
-
+    ll(n - 1 - i) = gcd(ll(n - i), a(n - 1 - i))
   }
-  rr = rr.reverse
 
-  var ans = List(rr(rr.length - 2), ll(1)).max
+  var ans = List(rr(n - 2), ll(1)).max
+
   (1 until n - 1).foreach { i =>
-    ans = List(ans, gcd(rr(i - 1), ll(i + 1))).max
+    ans = max(ans, gcd(rr(i - 1), ll(i + 1)))
   }
+
   println(ans)
 }
