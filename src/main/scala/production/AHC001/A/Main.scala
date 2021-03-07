312a69a5fbeee2ad371484c09f18db0bcbd51f8f
ik-fib <ik11235@gmail.com>
2021-03-07 09:43:59 +0900

    AHC001 update

--- src/main/scala/production/AHC001/A/Main.scala
+++ src/main/scala/production/AHC001/A/Main.scala
@@ -2,8 +2,33 @@ package production.AHC001.A
 
 object Main extends App {
   val n = scala.io.StdIn.readLine.toInt
-  (1 to n).foreach { i =>
+  var data: List[(Int, Int)] = List()
+  var xlist: List[Int] = List()
+  var ylist: List[Int] = List()
+
+  (1 to n).foreach { _ =>
     val Array(x, y, r) = scala.io.StdIn.readLine.split(" ").map(_.toInt)
-    println(x + " " + y + " " + (x + 1) + " " + (y + 1))
+    data = data :+ ((x, y))
+    xlist = xlist :+ x
+    ylist = ylist :+ y
+    //println(x + " " + y + " " + (x + 1) + " " + (y + 1))
+  }
+  xlist = xlist :+ 10001
+  ylist = ylist :+ 10001
+
+  xlist = xlist.sorted
+  ylist = ylist.sorted
+
+  data.foreach { dd =>
+    val x = dd._1
+    val y = dd._2
+
+    val xx = xlist.filter(_ > x).min - 1
+    val yy = ylist.filter(_ > y).min - 1
+    if ((x == xx) || (y == yy)) {
+      println(x + " " + y + " " + (x + 1) + " " + (y + 1))
+    } else {
+      println(x + " " + y + " " + xx + " " + yy)
+    }
   }
 }
